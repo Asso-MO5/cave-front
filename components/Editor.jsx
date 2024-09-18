80573ceebd6312f48f9e18ccc959bcde056590c5
Kazerlelutin <b@bouteiller.contact>
2024-09-18 02:43:08 +0200

    fix package

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -3,38 +3,11 @@ import { BlockNoteView } from '@blocknote/mantine'
 import { useCreateBlockNote } from '@blocknote/react'
 import { useEffect, useId, useState } from 'react'
 import { locales } from '@blocknote/core'
-import * as Y from 'yjs'
-import { WebsocketProvider } from 'y-websocket'
-import { decimalToHex } from '@/utils/decimalToHex'
 import { editorInitialContent } from '@/utils/editor-initial-content'
 import { useDebounce } from '@/hooks/useDebounce'
 import { EditorRead } from './EditorRead'
 
-const doc = new Y.Doc()
-
-export function Editor({ onChange, id, defaultValue = '', session, disabled }) {
-  // ==== COLLABORATION ========================================
-  const wsProvider = session
-    ? new WebsocketProvider(
-        process.env.NEXT_PUBLIC_EDITOR_URL,
-        `editor-${id}`,
-        doc
-      )
-    : undefined
-
-  const collaboration = session
-    ? {
-        provider: wsProvider,
-        fragment: doc.getXmlFragment('document-store'),
-        user: {
-          name: session.user.name,
-          color: decimalToHex(
-            session.user.roles.filter((r) => r.color > 0)[0].color
-          ),
-        },
-      }
-    : undefined
-
+export function Editor({ onChange, id, defaultValue = '', disabled }) {
   // ==== EDITOR ========================================
   const idGen = useId()
   const [init, setInit] = useState(false)
@@ -52,7 +25,6 @@ export function Editor({ onChange, id, defaultValue = '', session, disabled }) {
   const editor = useCreateBlockNote({
     theme: 'light',
     dictionary: locales.fr,
-    // collaboration,
     toolbar: ['bold', 'italic', 'underline', 'link', 'image', 'quote', 'code'],
     initialContent: blocks,
   })
