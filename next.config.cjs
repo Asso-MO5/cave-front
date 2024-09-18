83dcd2c773af893890778403164313476ad57ee9
Kazerlelutin <b@bouteiller.contact>
2024-09-18 03:20:03 +0200

    minify

--- next.config.cjs
+++ next.config.cjs
@@ -6,6 +6,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  swcMinify: true,
+}
 
 module.exports = nextConfig
