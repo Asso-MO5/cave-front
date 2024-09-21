21b6d97086a6191ffe23b32312251afd570ae4ac
Kazerlelutin <b@bouteiller.contact>
2024-09-21 22:26:00 +0200

    fix counter

--- components/cartel/cartel.utils.js
+++ components/cartel/cartel.utils.js
@@ -1,17 +1,17 @@
 export const PRINT_TYPES = [
   {
     name: 'carte',
-    short_description: 100,
+    long_short_description: 450,
   },
   {
     name: 'a4',
-    short_description: 100,
+    long_short_description: 200,
     long_description_fr: 600,
     long_description_en: 600,
   },
   {
     name: 'rollup',
-    short_description: 100,
+    long_short_description: 200,
     long_description_fr: 600,
     long_description_en: 600,
   },
@@ -20,17 +20,17 @@ export const PRINT_TYPES = [
 export const PRINT_TYPES_GAME = [
   {
     name: 'carte',
-    short_description: 100,
+    long_short_description: 100,
   },
   {
     name: 'a4',
-    short_description: 100,
+    short_description: 200,
     long_description_fr: 1000,
     long_description_en: 1000,
   },
   {
     name: 'rollup',
-    short_description: 100,
+    long_short_description: 200,
     long_description_fr: 1000,
     long_description_en: 1000,
   },
