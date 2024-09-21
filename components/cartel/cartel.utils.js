24a077c5233cb898bf1001645fbe92dca74b9482
Kazerlelutin <b@bouteiller.contact>
2024-09-21 12:13:47 +0200

    fix counts

--- components/cartel/cartel.utils.js
+++ components/cartel/cartel.utils.js
@@ -2,22 +2,17 @@ export const PRINT_TYPES = [
   {
     name: 'carte',
     short_description: 100,
-    long_description: 0,
-    description_rollup: 0,
-    description_en: 200,
   },
   {
     name: 'a4',
     short_description: 100,
-    long_description: 200,
-    description_rollup: 0,
-    description_en: 200,
+    long_description_fr: 600,
+    long_description_en: 600,
   },
   {
     name: 'rollup',
     short_description: 100,
-    long_description: 200,
-    description_rollup: 200,
-    description_en: 200,
+    long_description_fr: 600,
+    long_description_en: 600,
   },
 ]
