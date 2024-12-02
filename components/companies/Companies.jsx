9e24426b2fed9d73cb4cac1ea60efdcae1c8f7ed
Kazerlelutin <b@bouteiller.contact>
2024-12-02 23:56:05 +0100

    nettoyage

--- components/companies/Companies.jsx
+++ components/companies/Companies.jsx
@@ -1,17 +1,11 @@
 'use client'
 import { StretchBox } from '@/ui/StretchBox'
 
-import { Modal as ModalCreate } from './create/Modal'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { Table } from '@/ui/table/Table'
 import { fetcher } from '@/utils/fetcher'
-import { Checkbox } from '@/ui/Checkbox'
-import { ACTIVITIES_COMPANY, ITEM_TYPE_TITLE } from '@/utils/constants'
-import { TrashIcon } from '@/ui/icon/TrashIcon'
-import { Modal } from '@/ui/Modal'
-import { ExportBtn } from '../items/ExportBtn'
-import { PrintSelector } from '../items/PrintSelector'
+import { ACTIVITIES_COMPANY } from '@/utils/constants'
 import { StatusChip } from '@/ui/StatusChip'
 import Link from 'next/link'
 
@@ -21,24 +15,6 @@ export function Companies() {
   const [loading, setLoading] = useState(true)
   const [data, setData] = useState([])
   const [total, setTotal] = useState(0)
-  const [selectedAll, setSelectedAll] = useState(false)
-  const [selectedTotal, setSelectedTotal] = useState(false)
-
-  const handleSelectAll = () => {
-    setSelectedAll(!selectedAll)
-    data.forEach((item) => (item.selected = !selectedAll))
-  }
-
-  async function handleDelete({ id }) {
-    const ctrl = new AbortController()
-    const oldData = data
-    setData(data.filter((item) => item.id !== id))
-    try {
-      await fetcher.delete(`/items/${id}`, ctrl.signal)
-    } catch (e) {
-      setData(oldData)
-    }
-  }
 
   async function handleFetch() {
     setLoading(true)
