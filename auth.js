f04e65e0db91b9452c6397d20328447b9e9dfff1
Kazerlelutin <b@bouteiller.contact>
2025-02-19 11:21:04 +0100

    test

--- auth.js
+++ auth.js
@@ -73,6 +73,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         .encrypt(secret)
 
       try {
+        /*
         const caveApi = await fetch(
           process.env.NEXT_PUBLIC_API_URL + '/auth/login',
           {
@@ -84,8 +85,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         const caveApiJson = await caveApi.json()
         session.api_token = caveApiJson.auth
 
-        session.routes = caveApiJson.routes
+        // session.routes = caveApiJson.routes
         session.user.id = member.user.id
+
+        */
 
         return session
       } catch (e) {
